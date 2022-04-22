0adf56cc7e23dea8930bf249a6e179e42b34785d
johnj974 <jjdug@hotmail.com>
2022-04-22 11:17:56 +0100

    FooterAdded+StyledNavbarBrandAddedBaseColorsAdded

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  //.
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toContactPage() {
+    console.log('working');
+    this.router.navigate(['/contact']);
   }
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {}
 }
