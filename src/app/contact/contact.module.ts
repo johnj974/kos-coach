6a9eaf4a4e4de490efdd4d0c58c4deecbb2bfe73
johnj974 <jjdug@hotmail.com>
2022-04-24 13:19:35 +0100

    ContactInfoAdded+BasicStyles+ColorChangeToNavbar

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactMainComponent } from './contact-main/contact-main.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 
 @NgModule({
   declarations: [
-    ContactMainComponent
+    ContactMainComponent,
+    ContactFormComponent,
+    ContactInfoComponent
   ],
   imports: [
     CommonModule,
