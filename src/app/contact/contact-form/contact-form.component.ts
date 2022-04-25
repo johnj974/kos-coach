d5d4d68230add226d311a6b4613f5799e9bfbf32
johnj974 <jjdug@hotmail.com>
2022-04-25 14:02:34 +0100

    contactFormAdded

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
+  //.
+  contactForm: FormGroup;
 
-  constructor() { }
+  submitForm() {
+    console.log(this.contactForm);
+
+    setTimeout(() => {
+      this.contactForm.reset();
+    }, 5000);
+  }
+  constructor() {}
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('[0-9]{10}'),
+      ]),
+      service: new FormControl(null, Validators.required),
+      message: new FormControl(null, Validators.required),
+    });
   }
-
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,17 +5,14 @@ import { ContactRoutingModule } from './contact-routing.module';
 import { ContactMainComponent } from './contact-main/contact-main.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ContactMainComponent,
     ContactFormComponent,
-    ContactInfoComponent
+    ContactInfoComponent,
   ],
-  imports: [
-    CommonModule,
-    ContactRoutingModule
-  ]
+  imports: [CommonModule, ContactRoutingModule, ReactiveFormsModule],
 })
-export class ContactModule { }
+export class ContactModule {}
