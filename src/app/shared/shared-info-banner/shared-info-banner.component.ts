286810939957f2d7810159ac121127de0851fa19
johnj974 <jjdug@hotmail.com>
2022-09-08 17:14:28 +0100

    infoBannerComponentAdded+Content+Style

---
+++ src/app/shared/shared-info-banner/shared-info-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-info-banner',
+  templateUrl: './shared-info-banner.component.html',
+  styleUrls: ['./shared-info-banner.component.css']
+})
+export class SharedInfoBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedContactComponent } from './shared-contact/shared-contact.component';
 import { SplitLastPipe } from './reduceText.pipe';
+import { SharedInfoBannerComponent } from './shared-info-banner/shared-info-banner.component';
 
 @NgModule({
-  declarations: [SharedContactComponent, SplitLastPipe],
+  declarations: [
+    SharedContactComponent,
+    SplitLastPipe,
+    SharedInfoBannerComponent,
+  ],
   imports: [CommonModule],
-  exports: [SharedContactComponent, SplitLastPipe],
+  exports: [SharedContactComponent, SplitLastPipe, SharedInfoBannerComponent],
 })
 export class SharedModule {}
